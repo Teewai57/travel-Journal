0fd50ea52f625edf8decacc958b9311cd516f06f
Teewai57 <bobtoyoabasi@gmail.com>
2022-06-14 11:52:09 +0100

    adjusted data

--- src/data.js
+++ src/data.js
@@ -11,18 +11,18 @@ export default [
     {
         id: 2,
         imageUrl:  "https://media.istockphoto.com/photos/beautiful-woman-with-perfect-curly-hairs-picture-id1335039120?k=20&m=1335039120&s=612x612&w=0&h=5r1d0fwh0w23npQAQIXZNAIpuFBt4cFnSIjGKLGeH24=",
-        name: "John Doe",
-        occupation: "Devops Engineer",
-        email: "johndoe.website",
+        name: "Kimberly Nathan",
+        occupation: "Digital Marketer",
+        email: "kimberly.ng",
         gender: "female"
 
        },
     {
         id: 3,
         imageUrl:  "https://media.istockphoto.com/photos/young-brown-haired-beautiful-model-with-wavy-dense-dyed-long-hair-picture-id1317793666?k=20&m=1317793666&s=612x612&w=0&h=c0kZReCc5pgoN0Y4oN59-T1T_Jcq9rPtDnORB08ntCE=",
-        name: "John Doe",
-        occupation: "Devops Engineer",
-        email: "johndoe.website",
+        name: "Sophia Walters",
+        occupation: "Software Engineer",
+        email: "walters.website",
         gender: "female"
 
 
@@ -30,9 +30,9 @@ export default [
     {
         id: 4,
         imageUrl:  "https://media.istockphoto.com/photos/smiling-man-outdoors-in-the-city-picture-id1179420343?k=20&m=1179420343&s=612x612&w=0&h=G2UGMVSzAXGAQs3pFZpvWlHNRAzwPIWIVtSOxZHsEuc=",
-        name: "John Doe",
-        occupation: "Devops Engineer",
-        email: "johndoe.website",
+        name: "James Ealy",
+        occupation: "Front-end Developer",
+        email: "Ealy.ng",
         gender: "male"
 
 
@@ -40,9 +40,9 @@ export default [
     {
         id: 5,
         imageUrl:  "https://media.istockphoto.com/photos/cool-african-man-picture-id544358094?k=20&m=544358094&s=612x612&w=0&h=QO2YDUbPLyv0BfHQbEHMrSLcuT3mrZ-HpO8ynbEstf4=",
-        name: "John Doe",
+        name: "Mike Kevin",
         occupation: "Devops Engineer",
-        email: "johndoe.website",
+        email: "kevin.website",
         gender: "male"
 
 
@@ -50,9 +50,9 @@ export default [
     {
         id: 6,
         imageUrl:  "https://media.istockphoto.com/photos/beautiful-woman-picture-id1021162344?k=20&m=1021162344&s=612x612&w=0&h=zVsn5v6F07ifGKzVXbqbjweoDz7mC7G-um_s40OXwqo=",
-        name: "John Doe",
+        name: "Amanda Richards",
         occupation: "Devops Engineer",
-        email: "johndoe.website",
+        email: "Amanda.website",
         gender: "female"
 
 
